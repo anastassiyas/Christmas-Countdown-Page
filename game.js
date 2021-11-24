5dc2c7e88f67e07daf9f13932e03dbf81db58cdb
anastassiya.savostina <a.savostina@icloud.com>
2021-11-24 00:05:48 -0700

    Adding files

--- game.js
+++ game.js
@@ -1,16 +1,16 @@
 function elfName()
 {
 var myElfName= new Array(
-"You'l be rich",
-"You'll	be	famous",
-"You'll	fall in	love",
-"You'll	be	happy",
-"Good fortune will be yours",
-"You will have many friends",
-"Do a good deed today",
-"Someone will call you today",
-"You will go to a party soon",
-"Be careful on Tuesday");
+"Adorellan Virdove",
+"Feno Daerie",
+"Selanar Ergeiros",
+"Lathlaeril Wynpetor",
+"Essaerae Gilphyra",
+"Holone Dalamin",
+"Darshee Wranfina",
+"Ava Sylnan",
+"Edea Arafir",
+"Meriel Lulee");
 var random = myElfName[Math.floor(Math.random() * myElfName.length)];
 //alert(random);
 document.getElementById("message").innerHTML=random;
